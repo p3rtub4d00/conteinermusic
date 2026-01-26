551c9705bea520ada97a99bcf2ea0359eaaced00
p3rtub4d00 <containerbeerpvh@gmail.com>
2026-01-26 08:01:05 -0400

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'admin-conteiner-v1';
+const urlsToCache = [
+  '/admin.html',
+  '/css/admin.css',
+  '/js/admin.js',
+  '/images/logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
